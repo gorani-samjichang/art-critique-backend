0615a4d688a36eac157393d4eae8692d4dc8e2c5
opi6opi6 <opi6@hanmail.net>
2024-06-18 12:30:09 +0900

    init

---
+++ src/main/java/com/gorani_samjichang/art_critique/ArtCritiqueApplication.java
@@ -0,0 +1,13 @@
+package com.gorani_samjichang.art_critique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtCritiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtCritiqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gorani_samjichang/art_critique/ArtCritiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorani_samjichang.art_critique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtCritiqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
