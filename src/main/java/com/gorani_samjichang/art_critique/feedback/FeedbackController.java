90ab2536342af87f8ecf744371cdbacd266dc589
opi6opi6 <opi6@hanmail.net>
2024-07-04 14:00:46 +0900

    Feature: pinia도입

--- src/main/java/com/gorani_samjichang/art_critique/feedback/FeedbackController.java
+++ src/main/java/com/gorani_samjichang/art_critique/feedback/FeedbackController.java
@@ -1,9 +1,9 @@
 package com.gorani_samjichang.art_critique.feedback;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 @RestController
@@ -15,5 +15,11 @@ public class FeedbackController {
         for (int i = 0; i < 15; i++)
             data.add(String.format("https://picsum.photos/id/" + (int)(Math.floor(Math.random() * (100 + 1))) + "/" + (int)((Math.floor(Math.random() * (10 + 1)) + 5)*20) + "/" + (int)((Math.floor(Math.random() * (10 + 1)) + 5)*20)));
         return data;
+    }
+
+    @PostMapping("/requestFeedback")
+    String requestFeedback(@RequestParam("image") MultipartFile imageFile) throws IOException {
+        System.out.println(imageFile);
+        return "왔음!";
     }
 }
