7072bbc7becb5820673220e26d41e8cbe5dd4662
AmenPark <puzzlistpam@gmail.com>
2024-07-25 13:00:18 +0900

    Feature: add MemberDto

---
+++ src/main/java/com/gorani_samjichang/art_critique/member/MemberDto.java
@@ -0,0 +1,14 @@
+package com.gorani_samjichang.art_critique.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDto {
+    private String email;
+    private String profile;
+    private String role;
+    private String level;
+    private String nickname;
+}
---
+++ src/main/java/com/gorani_samjichang/art_critique/member/MemberService.java
@@ -0,0 +1,2 @@
+package com.gorani_samjichang.art_critique.member;public class MemberService {
+}
