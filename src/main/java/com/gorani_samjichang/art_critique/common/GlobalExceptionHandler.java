e942610f68912a37033cfbbfd5b1c7ce9c75a5ae
AmenPark <puzzlistpam@gmail.com>
2024-07-28 02:56:34 +0900

    Feature: 에러사항 추가

--- src/main/java/com/gorani_samjichang/art_critique/common/GlobalExceptionHandler.java
+++ src/main/java/com/gorani_samjichang/art_critique/common/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.gorani_samjichang.art_critique.common;
 
+import com.gorani_samjichang.art_critique.common.exceptions.UserNotFoundException;
+import com.gorani_samjichang.art_critique.common.exceptions.UserNotValidException;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
@@ -9,20 +12,20 @@ import org.springframework.web.client.HttpClientErrorException;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(HttpClientErrorException.Forbidden.class)
-    public String handleUnauthenticatedException(HttpServletResponse response, final HttpClientErrorException.Forbidden e) {
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        return e.getMessage();
-    }
-
-    @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
-    public String handleUnauthenticatedException(HttpServletResponse response, final HttpClientErrorException.Unauthorized e) {
+    @ExceptionHandler(UserNotFoundException.class)
+    public String handleNoUser(HttpServletResponse response, final HttpClientErrorException.Forbidden e) {
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         return e.getMessage();
     }
 
-    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
-    public String handleBadRequestException(HttpServletResponse response, final HttpClientErrorException.BadRequest e) {
+    @ExceptionHandler(UserNotValidException.class)
+    public String handleInvalidUser(HttpServletResponse response, final HttpClientErrorException.Unauthorized e) {
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public String handleNullEssentialParameter(HttpServletResponse response, final HttpClientErrorException.BadRequest e) {
         response.setStatus(HttpStatus.BAD_REQUEST.value());
         return e.getMessage();
     }
