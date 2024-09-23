96e3f7760c53b63fb743a651eac60ca3b7a21e2f
opi6opi6 <opi6@hanmail.net>
2024-09-23 19:55:48 +0900

    Feature: 배포의 시작

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@
  # 디렉터리가 존재하지 않으면 생성
  if [ ! -d "$DIRECTORY" ]; then
      mkdir "$DIRECTORY"
-
+ fi
  nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME 1>nohup/stdout.txt 2>nohup/stderr.txt &
  sleep 2
 
