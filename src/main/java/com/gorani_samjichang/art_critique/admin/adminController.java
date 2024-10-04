513cd1d3e2ea2f836628b38781167ab8cef8a10f
okane <okanekudasai6@gmail.com>
2024-10-05 07:48:37 +0900

    Feature: 관리자콜솔-멤버 리스트 뽑기

--- src/main/java/com/gorani_samjichang/art_critique/admin/adminController.java
+++ src/main/java/com/gorani_samjichang/art_critique/admin/adminController.java
@@ -1,5 +1,7 @@
 package com.gorani_samjichang.art_critique.admin;
 
+import com.gorani_samjichang.art_critique.member.MemberDto;
+import com.gorani_samjichang.art_critique.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -7,12 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class adminController {
+    final MemberRepository memberRepository;
     @GetMapping("/isAdmin")
     ResponseEntity<Void> isAmdin() {
         return new ResponseEntity<>(HttpStatusCode.valueOf(200));
+    }
+
+    @GetMapping("/userList")
+    ResponseEntity<List<MemberDto>> userList() {
+        List<MemberDto> list = memberRepository.getAllMemberDto();
+        return new ResponseEntity<>(list, HttpStatusCode.valueOf(200));
     }
 }
