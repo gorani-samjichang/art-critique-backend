a651f5c3bba6b0b775d22f4b46d0b73e53b2b230
okane <okanekudasai6@gmail.com>
2024-11-01 06:17:34 +0900

    Chore: 네이버페이 결제창 띄우고 결과 받기

--- src/main/java/com/gorani_samjichang/art_critique/credit/CreditController.java
+++ src/main/java/com/gorani_samjichang/art_critique/credit/CreditController.java
@@ -1,15 +1,25 @@
 package com.gorani_samjichang.art_critique.credit;
 
+import com.gorani_samjichang.art_critique.appConstant.FeedbackState;
+import com.gorani_samjichang.art_critique.feedback.FeedbackEntity;
+import com.gorani_samjichang.art_critique.feedback.FeedbackResultEntity;
 import com.gorani_samjichang.art_critique.member.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/credit")
@@ -17,6 +27,7 @@ import java.util.List;
 public class CreditController {
     final CreditRepository creditRepository;
     final CreditUsedHistoryRepository creditUsedHistoryRepository;
+    final WebClient.Builder webClientBuilder;
     @GetMapping("/payment-history")
     HashMap<String, Object> paymentHistory(@AuthenticationPrincipal CustomUserDetails userDetails) {
         Integer remainPrepaymentCredit = creditRepository.sumOfPrepaymentCredit(userDetails.getUid());
@@ -50,6 +61,57 @@ public class CreditController {
         }
 
         return dto;
+    }
+
+    @Value("${naver_pay.client_id}")
+    String clientId;
+    @Value("${naver_pay.client_secret}")
+    String clientSecret;
+    @Value("${naver_pay.chain_id}")
+    String chainId;
+    @GetMapping("/pre/payment-result/{paymentId}")
+    String paymentResult(@PathVariable String paymentId) {
+        String body = UriComponentsBuilder.newInstance()
+                .queryParam("paymentId", paymentId)
+                .build()
+                .toUriString()
+                .substring(1);
+        String r = webClientBuilder.build()
+                .post()
+                .uri("https://dev.apis.naver.com/naverpay-partner/naverpay/payments/v2.2/apply/payment")
+                .header("X-Naver-Client-Id", clientId)
+                .header("X-Naver-Client-Secret", clientSecret)
+                .header("X-NaverPay-Chain-Id", chainId)
+                .header("X-NaverPay-Idempotency-Key", String.valueOf(UUID.randomUUID()))
+                .contentType(org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED)
+                .bodyValue(body)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        return r;
+    }
+
+    @GetMapping("/sub/payment-result/{reserveId}/{tempReceiptId}")
+    String paymentResult(@PathVariable String reserveId, @PathVariable String tempReceiptId) {
+        String body = UriComponentsBuilder.newInstance()
+                .queryParam("reserveId", reserveId)
+                .queryParam("tempReceiptId", tempReceiptId)
+                .build()
+                .toUriString()
+                .substring(1);
+        String r = webClientBuilder.build()
+                .post()
+                .uri("https://dev.apis.naver.com/naverpay-partner/naverpay/payments/recurrent/regist/v1/approval")
+                .header("X-Naver-Client-Id", clientId)
+                .header("X-Naver-Client-Secret", clientSecret)
+                .header("X-NaverPay-Chain-Id", chainId)
+                .header("X-NaverPay-Idempotency-Key", String.valueOf(UUID.randomUUID()))
+                .contentType(org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED)
+                .bodyValue(body)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        return r;
     }
 
     CreditDto convertEntityToDto(CreditEntity entity) {
