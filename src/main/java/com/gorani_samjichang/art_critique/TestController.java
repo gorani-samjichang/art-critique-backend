5585802030b85b015983a49807925f524adf0647
opi6opi6 <opi6@hanmail.net>
2024-11-09 09:39:30 +0900

    Chore: 빌드 테스트

--- src/main/java/com/gorani_samjichang/art_critique/TestController.java
+++ src/main/java/com/gorani_samjichang/art_critique/TestController.java
@@ -60,71 +60,71 @@ public class TestController {
             creditRepository.save(c2);
             memberRepository.save(admin);
         }
-//        if (innerStudyFieldRepository.count() == 0 && innerStudyCategoryRepository.count() == 0) {
-//            String [] fieldList = {
-//                    "좋은 드로잉을 위한 기본, 자세와 선",
-//                    "사실적인 드로잉을 위해 면과 명암을 다루는 기술",
-//                    "드로잉에서 가장 중요한 관찰의 원리와 올바른 관찰법",
-//                    "효과적이고 완벽한 드로잉을 위한 계측과 평면화",
-//                    "실전 드로잉을 위한 핵심 기법들"
-//            };
-//            String [] [] categoryList = {
-//                    {
-//                            "자신에게 숨겨져 있는 드로잉 본능을 꺼내라",
-//                            "다양한 드로잉의 세상",
-//                            "안정된 자세를 만들기 위한 7가지 규칙",
-//                            "좋은 선을 만드는 연필 잡기 방법",
-//                            "자연스러운 선 긋기를 위한 관절의 사용법",
-//                            "직선과 곡선 긋기의 정석",
-//                            "필압을 느끼며 선 긋기",
-//                            "자연스러운 선을 구사하는 방법",
-//                            "선의 모든 것"
-//                    },
-//                    {
-//                            "그림의 소재를 선택할 때 주의할 점",
-//                            "윤곽선으로 그리기와 면으로 그리기",
-//                            "면에 대한 이해와 활용",
-//                            "명암을 다루는 기술"
-//                    },
-//                    {
-//                            "눈의 다양한 특성과 시각에 대한 올바른 이해",
-//                            "관찰에 대한 이해와 관찰력의 향상",
-//                            "두 가지 관찰법",
-//                            "보이는 대로 관찰하기",
-//                            "계측을 이용하여 보이는 대로 관찰하기",
-//                            "해체를 이용하여 보이는 대로 관찰하기",
-//                            "보이는 대로 관찰하는 방법에 대한 의문",
-//                            "보이는 대로 관찰하기의 장점과 한계",
-//                            "이해하며 관찰하기",
-//                            "이해하며 관찰하기로 드로잉하기",
-//                            "이해하며 관찰하기의 장점과 한계",
-//                            "보이는 대로 그리기 + 이해하며 그리기",
-//                    },
-//                    {
-//                            "계측의 기본 원리와 10가지 계측 기술",
-//                            "평면화란 무엇인가?",
-//                            "평면화의 원리와 효과를 정확히 이해하기",
-//                            "대상을 평면화하는 가장 쉬운 3가지 방법",
-//                    },
-//                    {
-//                            "그려야 할 것과 생략해도 되는 것을 구분하는 방법",
-//                            "효과적인 생략을 위한 5가지 기법",
-//                            "효과적인 강조를 위한 5가지 기법",
-//                            "대상의 윤곽선 처리",
-//                            "드로잉에 심미적 효과를 덧입히는 8가지 기법",
-//                    }
-//            };
-//            for (int i = 0; i < fieldList.length; i++) {
-//                InnerStudyField field = InnerStudyField.builder().categoryTitle(fieldList[i]).build();
-//                innerStudyFieldRepository.save(field);
-//                for (String j : categoryList[i]) {
-//                    InnerStudyCategory category = InnerStudyCategory.builder().categoryName(j).field(field).build();
-//                    innerStudyCategoryRepository.save(category);
-//                    field.addDetail(category);
-//                }
-//                innerStudyFieldRepository.save(field);
-//            }
-//        }
+        if (innerStudyFieldRepository.count() == 0 && innerStudyCategoryRepository.count() == 0) {
+            String [] fieldList = {
+                    "좋은 드로잉을 위한 기본, 자세와 선",
+                    "사실적인 드로잉을 위해 면과 명암을 다루는 기술",
+                    "드로잉에서 가장 중요한 관찰의 원리와 올바른 관찰법",
+                    "효과적이고 완벽한 드로잉을 위한 계측과 평면화",
+                    "실전 드로잉을 위한 핵심 기법들"
+            };
+            String [] [] categoryList = {
+                    {
+                            "자신에게 숨겨져 있는 드로잉 본능을 꺼내라",
+                            "다양한 드로잉의 세상",
+                            "안정된 자세를 만들기 위한 7가지 규칙",
+                            "좋은 선을 만드는 연필 잡기 방법",
+                            "자연스러운 선 긋기를 위한 관절의 사용법",
+                            "직선과 곡선 긋기의 정석",
+                            "필압을 느끼며 선 긋기",
+                            "자연스러운 선을 구사하는 방법",
+                            "선의 모든 것"
+                    },
+                    {
+                            "그림의 소재를 선택할 때 주의할 점",
+                            "윤곽선으로 그리기와 면으로 그리기",
+                            "면에 대한 이해와 활용",
+                            "명암을 다루는 기술"
+                    },
+                    {
+                            "눈의 다양한 특성과 시각에 대한 올바른 이해",
+                            "관찰에 대한 이해와 관찰력의 향상",
+                            "두 가지 관찰법",
+                            "보이는 대로 관찰하기",
+                            "계측을 이용하여 보이는 대로 관찰하기",
+                            "해체를 이용하여 보이는 대로 관찰하기",
+                            "보이는 대로 관찰하는 방법에 대한 의문",
+                            "보이는 대로 관찰하기의 장점과 한계",
+                            "이해하며 관찰하기",
+                            "이해하며 관찰하기로 드로잉하기",
+                            "이해하며 관찰하기의 장점과 한계",
+                            "보이는 대로 그리기 + 이해하며 그리기",
+                    },
+                    {
+                            "계측의 기본 원리와 10가지 계측 기술",
+                            "평면화란 무엇인가?",
+                            "평면화의 원리와 효과를 정확히 이해하기",
+                            "대상을 평면화하는 가장 쉬운 3가지 방법",
+                    },
+                    {
+                            "그려야 할 것과 생략해도 되는 것을 구분하는 방법",
+                            "효과적인 생략을 위한 5가지 기법",
+                            "효과적인 강조를 위한 5가지 기법",
+                            "대상의 윤곽선 처리",
+                            "드로잉에 심미적 효과를 덧입히는 8가지 기법",
+                    }
+            };
+            for (int i = 0; i < fieldList.length; i++) {
+                InnerStudyField field = InnerStudyField.builder().categoryTitle(fieldList[i]).build();
+                innerStudyFieldRepository.save(field);
+                for (String j : categoryList[i]) {
+                    InnerStudyCategory category = InnerStudyCategory.builder().categoryName(j).field(field).build();
+                    innerStudyCategoryRepository.save(category);
+                    field.addDetail(category);
+                }
+                innerStudyFieldRepository.save(field);
+            }
+        }
     }
 
     @GetMapping("/hello")
